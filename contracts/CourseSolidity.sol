996b10273f1f91872bb1b639f54a382c3622f37d
Chris Williams <chris@innotrade.io>
2018-07-07 23:33:51 -0600

    modify contract with event and modifiers

--- contracts/CourseSolidity.sol
+++ contracts/CourseSolidity.sol
@@ -4,13 +4,22 @@ contract CourseSolidity {
     
     string fName;
     uint age;
+    address owner;
     
+    constructor(CourseSolidity) public {
+        owner = msg.sender;
+    }
+    
+    modifier onlyOwner{
+        require(msg.sender == owner);
+        _;
+    }
     event Instructor (
         string name,
         uint age
     );
     
-   function setInstructor(string _fName, uint _age) public {
+   function setInstructor(string _fName, uint _age) onlyOwner public {
        fName = _fName;
        age = _age;
        emit Instructor(_fName, _age);
